ec3e58e0b99dcacbb4bcd29a6d290cc3b60c5414
Lars_und_so <46791248+Larsundso@users.noreply.github.com>
2022-09-25 14:56:47 +0200

    switch to nuxt 3

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,21 +1,15 @@
+<script lang="ts">
+import HomePage from "./HomePage.vue";
+
+export default {
+  components: {
+    HomePage,
+  },
+};
+</script>
+
 <template>
   <div>
-    <NavBar />
-    <NuxtLink to="/">Hello</NuxtLink>
-    <NuxtLink to="*"><NotFound /></NuxtLink>
-    <PageFooter />
+    <HomePage />
   </div>
 </template>
-
-<script lang="ts">
-import Vue from 'vue'
-import NotFound from './NotFound.vue'
-import PageFooter from '~/components/PageFooter.vue'
-import NavBar from '~/components/NavBar.vue'
-import '~/assets/main.css'
-
-export default Vue.extend({
-  name: 'IndexPage',
-  components: { PageFooter, NavBar, NotFound },
-})
-</script>
